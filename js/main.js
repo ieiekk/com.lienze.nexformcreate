45588f60d006ed3e4b5c2d2d19fedc63cf0d0c36
ieiekk <ieie10715@gmail.com>
2022-08-05 17:00:18 +0800

    0805

--- js/main.js
+++ js/main.js
@@ -1,105 +1,141 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global $, window, location, CSInterface, SystemPath, themeManager*/
 
-// (function () {
-//     'use strict';
-
-//     var csInterface = new CSInterface();
-
-//     function init() {
-
-//         initColors();
-
-//         $("#btn_test").click(function () {
-//             csInterface.evalScript('sayHello()');
-//         });
-//     }
-
-//     init();
-
-// }());
-
-// function quests() {
-//   return [
-//     {
-//       uuid: uuidv4(),
-//       form: {
-//         title: 'Title',
-//         description: 'Description',
-//         type: 'singleLineText',
-//         value: '請輸入文字內容',
-//       },
-//       AE: {
-//         comp: 'Comp',
-//         layer: 'Layer',
-//         prop: 'Property',
-//       },
-//     },
-//     {
-//       uuid: uuidv4(),
-//       form: {
-//         title: 'Title',
-//         description: 'Description',
-//         type: 'multiLineText',
-//         value: '請輸入文字內容',
-//       },
-//       AE: {
-//         comp: 'Comp',
-//         layer: 'Layer',
-//         prop: 'Property',
-//       },
-//     },
-//     {
-//       uuid: uuidv4(),
-//       form: {
-//         title: 'Title',
-//         description: 'Description',
-//         type: 'selection',
-//         value: ['選項一', '選項二', '選項三'],
-//       },
-//       AE: {
-//         comp: 'Comp',
-//         layer: 'Layer',
-//         prop: 'Property',
-//       },
-//     },
-//   ];
-// }
 
 document.addEventListener('alpine:init', () => {
-  Alpine.data('Quests', () => ({
-    quests: [
-      // page 1
-      [
-        {
-          form: {
-            title: 'Title1',
-            description: 'Description1',
-            type: 'singleLineText',
-            options: ['選項一', '選項二', '選項三'],
-          },
-          AE: {
-            comp: 'Comp',
-            layer: 'Layer',
-            prop: 'Property',
-            value: 'AE source text',
-          },
-        },
-        {
-          form: {
-            title: 'Title2',
-            description: 'Description2',
-            type: 'multiLineText',
-            options: ['選項一', '選項二', '選項三'],
-          },
-          AE: {
-            comp: 'Comp',
-            layer: 'Layer',
-            prop: 'Property',
-            value: 'AE source text multi-line',
-          },
-        },
-        {
+  Alpine.data('Quests', function () {
+    return {
+      data: this.$persist({
+        formTitle: 'Test Title',
+        formDescription: 'Test Description',
+        quests: [
+          // page 1
+          [
+            {
+              uuid: uuid(),
+              form: {
+                title: 'Title1',
+                description: 'Description1',
+                type: 'singleLineText',
+                options: ['選項一', '選項二', '選項三'],
+              },
+              AE: {
+                type: 'data',
+                comp: 'Comp',
+                layer: 'Layer',
+                prop: 'Property',
+                value: 'AE source text',
+              },
+            },
+            {
+              uuid: uuid(),
+              form: {
+                title: 'Title2',
+                description: 'Description2',
+                type: 'multiLineText',
+                options: ['選項一', '選項二', '選項三'],
+              },
+              AE: {
+                type: 'data',
+                comp: 'Comp',
+                layer: 'Layer',
+                prop: 'Property',
+                value: 'AE source text multi-line',
+              },
+            },
+            {
+              uuid: uuid(),
+              form: {
+                type: 'data',
+                title: 'Title3',
+                description: 'Description3',
+                type: 'selection',
+                options: ['選項一', '選項二', '選項三'],
+              },
+              AE: {
+                type: 'data',
+                comp: 'Comp',
+                layer: 'Layer',
+                prop: 'Property',
+                value: 1,
+              },
+            },
+            {
+              uuid: uuid(),
+              form: {
+                title: 'Title4',
+                description: 'Description3',
+                type: 'file',
+                options: ['選項一', '選項二', '選項三'],
+              },
+              AE: {
+                type: 'video',
+                comp: 'Comp',
+                layer: 'Layer',
+                prop: 'Property',
+                value: '~/path/to/thisFile.mp4', // For footage, it's the filepath
+              },
+            },
+          ],
+          // page 2
+          [
+            {
+              uuid: uuid(),
+              form: {
+                title: 'Title1',
+                description: 'Description1',
+                type: 'singleLineText',
+                options: ['選項一', '選項二', '選項三'],
+              },
+              AE: {
+                type: 'data',
+                comp: 'Comp',
+                layer: 'Layer',
+                prop: 'Property',
+                value: 'AE source text',
+              },
+            },
+            {
+              uuid: uuid(),
+              form: {
+                title: 'Title2',
+                description: 'Description2',
+                type: 'multiLineText',
+                options: ['選項一', '選項二', '選項三'],
+              },
+              AE: {
+                type: 'data',
+                comp: 'Comp',
+                layer: 'Layer',
+                prop: 'Property',
+                value: 'AE source text multi-line',
+              },
+            },
+            {
+              uuid: uuid(),
+              form: {
+                title: 'Title3',
+                description: 'Description3',
+                type: 'selection',
+                options: ['選項一', '選項二', '選項三'],
+              },
+              AE: {
+                type: 'data',
+                comp: 'Comp',
+                layer: 'Layer',
+                prop: 'Property',
+                value: 1,
+              },
+            },
+          ],
+        ],
+      }),
+
+      currentPage: this.$persist(1),
+
+      addQuest() {
+        const emptyQuest = {
+          uuid: uuid(),
           form: {
             title: 'Title3',
             description: 'Description3',
@@ -107,91 +143,79 @@ document.addEventListener('alpine:init', () => {
             options: ['選項一', '選項二', '選項三'],
           },
           AE: {
+            type: 'data',
             comp: 'Comp',
             layer: 'Layer',
             prop: 'Property',
             value: 1,
           },
-        },
-        {
-          form: {
-            title: 'Title4',
-            description: 'Description3',
-            type: 'file',
-            options: ['選項一', '選項二', '選項三'],
-          },
-          AE: {
-            comp: 'Comp',
-            layer: 'Layer',
-            prop: 'Property',
-            value: '~/path/to/thisFile.mp4', // For footage, it's the filepath
-          },
-        },
-      ],
-      // page 2
-      [
-        {
-          form: {
-            title: 'Title1',
-            description: 'Description1',
-            type: 'singleLineText',
-            options: ['選項一', '選項二', '選項三'],
-          },
-          AE: {
-            comp: 'Comp',
-            layer: 'Layer',
-            prop: 'Property',
-            value: 'AE source text',
-          },
-        },
-        {
-          form: {
-            title: 'Title2',
-            description: 'Description2',
-            type: 'multiLineText',
-            options: ['選項一', '選項二', '選項三'],
-          },
-          AE: {
-            comp: 'Comp',
-            layer: 'Layer',
-            prop: 'Property',
-            value: 'AE source text multi-line',
-          },
-        },
-        {
-          form: {
-            title: 'Title3',
-            description: 'Description3',
-            type: 'selection',
-            options: ['選項一', '選項二', '選項三'],
-          },
-          AE: {
-            comp: 'Comp',
-            layer: 'Layer',
-            prop: 'Property',
-            value: 1,
-          },
-        },
-      ],
-    ],
+        };
 
-    currentPage: 1,
+        if (this.data.quests.length === 0) {
+          this.data.quests.push([emptyQuest]);
+        } else {
+          this.data.quests[this.currentPage - 1].push({
+            uuid: uuid(),
+            form: {
+              title: 'Title3',
+              description: 'Description3',
+              type: 'selection',
+              options: ['選項一', '選項二', '選項三'],
+            },
+            AE: {
+              type: 'data',
+              comp: 'Comp',
+              layer: 'Layer',
+              prop: 'Property',
+              value: 1,
+            },
+          });
+        }
+      },
 
-    addQuest() {
-      this.quests[this.currentPage - 1].push({
-        form: {
-          title: 'Title3',
-          description: 'Description3',
-          type: 'selection',
-          options: ['選項一', '選項二', '選項三'],
-        },
-        AE: {
-          comp: 'Comp',
-          layer: 'Layer',
-          prop: 'Property',
-          value: 1,
-        },
-      });
-    },
-  }));
+      addPage() {
+        this.data.quests.splice(this.currentPage, 0, []);
+        this.currentPage = this.currentPage + 1;
+      },
+
+      removePage() {
+        if (this.currentPage <= 1) return;
+        this.data.quests.splice(this.currentPage - 1, 1);
+        if (this.currentPage > this.data.quests.length) {
+          this.currentPage = this.data.quests.length;
+        }
+      },
+
+      submit() {
+        const modalBody = document.getElementById('modal-body');
+        modalBody.innerText = 'Submitting...';
+
+        setTimeout(() => {
+          alert(JSON.stringify(this.data));
+        }, 100);
+      },
+
+      clear() {
+        this.currentPage = 1;
+        this.data.formTitle = '';
+        this.data.formDescription = '';
+        this.data.quests = [[]];
+      },
+    };
+  });
 });
+
+function uuid() {
+  var d = Date.now();
+  if (
+    typeof performance !== 'undefined' &&
+    typeof performance.now === 'function'
+  ) {
+    d += performance.now(); //use high-precision timer if available
+  }
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+  });
+}
